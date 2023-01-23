4654c9058847f9e9129f2cee8128ebe51e3c4bd9
Shehla Kulsum <shehla1739@gmail.com>
2023-01-23 23:42:58 +0530

    added Teacher and Subject entities

---
+++ LearnersAcademy/src/main/java/org/ksk/learnersacademy/entities/Subject.java
@@ -0,0 +1,39 @@
+package org.ksk.learnersacademy.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Subject {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int sid;
+	private String name;
+	@ManyToOne
+	//set relationship 
+	private Teacher teacher;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+		
+}
---
+++ LearnersAcademy/src/main/java/org/ksk/learnersacademy/entities/Teacher.java
@@ -0,0 +1,75 @@
+package org.ksk.learnersacademy.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Teacher {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private int tid;
+	private String name;
+	private String address;
+	private String designation;
+	private String skill;
+	@OneToMany
+	//set relationship
+	private Set<Subject> subjects=new HashSet<>();	
+	
+	
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getSkill() {
+		return skill;
+	}
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+	public Set<Subject> getSubjects() {
+		return subjects;
+	}
+	public void setSubjects(Set<Subject> subjects) {
+		this.subjects = subjects;
+	}
+	//helper methods
+	public void addSubject(Subject subject) {
+		subjects.add(subject);	
+	}
+	public void removeSubject(Subject subject) {
+		subjects.remove(subject);
+	}
+}
+
+
+
+
+
